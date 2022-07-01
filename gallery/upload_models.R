1502f279df846a283b7864fe476f8db7beb4c13d
mateoensc <mmoreau016@ensc.fr>
2022-07-01 09:50:58 +0400

    :zap: save data and url with function

--- gallery/upload_models.R
+++ gallery/upload_models.R
@@ -11,9 +11,10 @@ base_dir <- "/data/outputs"
 dirs <- list.dirs(base_dir, full.names = FALSE, recursive = FALSE)
 
 #select only sites in site_info
-
+site_info$code
 #Add | in site_info$code "or term"
 patt <- paste(site_info$code, collapse = "|") 
+patt <- "ae|ib|su|ng|jr"
 # Choose file wich contains patt
 dirs <- grep(pattern = patt, dirs, value = TRUE) 
 
@@ -21,68 +22,59 @@ dirs <- grep(pattern = patt, dirs, value = TRUE)
 # read.csv("/data/outputs/sp_type.csv") 
 sp_type <- read.csv(paste0(base_dir, "/", "sp_type.csv"), header= TRUE)
 #Select files wich contain fmr
-samp_files <- grep("fmr", list.files(base_dir, pattern = "csv", full.names = TRUE), value = TRUE) 
-#lapply function return list
-samps <- lapply(samp_files, read.csv) 
-#Put col on right place
-samps[[3]][3] <- samps[[3]][4] 
-samps[[2]][3] <- samps[[2]][4]
+# samp_files <- grep("fmr", list.files(base_dir, pattern = "csv", full.names = TRUE), value = TRUE) 
+# #lapply function return list
+# samps <- lapply(samp_files, read.csv) 
+# #Put col on right place
+# samps[[3]][3] <- samps[[3]][4] 
+# samps[[2]][3] <- samps[[2]][4]
+# 
+# #Rename site
+# samps[[4]]$Site <- gsub("Aéroport_P1","Aeroport",samps[[4]]$Site)
+# samps[[4]]$Site <- gsub("Surprise_P2","Surprise",samps[[4]]$Site)  
+# samps[[2]]$Site <- gsub("sakouli","Sakouli",samps[[2]]$Site)
+# #lapply(samps, ncol)
+# 
+# samplings <- do.call(rbind, samps)
+# 
+# #cleaning
+# samplings$Site <- gsub("é", "e", samplings$Site)
+# samplings$Site <- gsub("'", "", samplings$Site)
+# samplings$Name <- gsub("Tag ","tag_", samplings$Name)
+# samplings <- samplings[substr(samplings$Name,1,4) == "tag_",] #keep only colonies
+# samplings$Name <- gsub("tag_","", samplings$Name)
+# samplings$Espèce <- str_to_sentence(samplings$Espèce)
+# sp_type$genus_sp <- str_to_sentence(sp_type$genus_sp)
+# samplings <- drop_na(samplings)
 
-#Rename site
-samps[[4]]$Site <- gsub("Aéroport_P1","Aeroport",samps[[4]]$Site)
-samps[[4]]$Site <- gsub("Surprise_P2","Surprise",samps[[4]]$Site)  
-samps[[2]]$Site <- gsub("sakouli","Sakouli",samps[[2]]$Site)
-#lapply(samps, ncol)
-
-samplings <- do.call(rbind, samps)
-
-#cleaning
-samplings$Site <- gsub("é", "e", samplings$Site)
-samplings$Site <- gsub("'", "", samplings$Site)
-samplings$Name <- gsub("Tag ","tag_", samplings$Name)
-samplings <- samplings[substr(samplings$Name,1,4) == "tag_",] #keep only colonies
-samplings$Name <- gsub("tag_","", samplings$Name)
-samplings$Espèce <- tolower(samplings$Espèce)
-sp_type$genus_sp <- tolower(sp_type$genus_sp)
-
-
-
-col_paths <- list.files("/data/colonies_stageMateo/cap_ecran_colonies", recursive = TRUE, full.names = TRUE)
-
-
-res <- lapply(dirs, function(d) {
-  # Iteration sur les dirs de outputs (avec regle s de selection)
-  #d = dirs[1:6]  
+samplingsfile <- read.csv("samplings.csv", header = TRUE)
+if (file.exists("data.csv")) {
+  data <- read.csv("data.csv")
+  dirs<- dirs[!is.element(dirs,data$id)]
+  data <- load("gallery/mod_urls.RData")
+}
+#d = dirs[1:6]
+iteration = dirs[1:4]
+new_res<- do.call(rbind,res <- lapply(iteration, function(d) {
+  # Iteration sur les dirs de outputs (avec regles de selection)
+  
   # Find .obj and texture
-  obj_path <-   grep("model.obj",list.files( paste0(base_dir,"/",dirs[1]), full.names = TRUE), value= TRUE)
-  texture_path <-   grep("model.jpg",list.files( paste0(base_dir,"/",dirs[1]), full.names = TRUE), value= TRUE)
+  obj_path <-   grep("model.obj",list.files( paste0(base_dir,"/",d), full.names = TRUE), value= TRUE)
+  texture_path <-   grep("model.jpg",list.files( paste0(base_dir,"/",d), full.names = TRUE), value= TRUE)
   #Split elements
   strings <- strsplit(d, "_")[[1]] 
   #name columns
   names(strings) <- c("site", "sampling", "colony_number") 
   #Take only code part like "ae"
   strings["site"] <- substr(strings["site"], 1, 2) 
-  
   site_name <-  site_info$site[site_info$code == strings["site"]]
-  
-  
-  
-  #init variables
-  session <- "session1"
-  #Find species Name with tag
-  
-  sp_name <- subset(samplings, Site == site_name & Name == strings['colony_number'])["Espèce"]
+  sp_name <- subset(samplingsfile, Site == site_name & Name == strings['colony_number'])["Espèce"]
   sp_name <- sp_name$Espèce
-  sp_name_maj <- stringr::str_to_title(sp_name)
-  #Find type with species name
-  type <- subset(sp_type, genus_sp == sp_name)
-  type <- type$LHT
-
+   
   
 # zip des model.obj et .jpeg
 #cherry-pick allows you to zip files with differents paths see relative path's doc
   zip_char <- paste0(d,".zip")
-  obj <- list.dirs(d, full.names = FALSE, recursive = FALSE)
   zip::zip(zipfile = zip_char ,c(obj_path,texture_path), mode = "cherry-pick")
   
   
@@ -95,7 +87,7 @@ res <- lapply(dirs, function(d) {
   sketch_tok <- read.table("gallery/sketchfab_api_token.txt")[1,1]
 
   mod_url <- upload(mod_path = reticulate::r_to_py(zip_char),
-                  mod_name = reticulate::r_to_py(sp_name_maj),
+                  mod_name = reticulate::r_to_py(sp_name),
                   api_token = reticulate::r_to_py(sketch_tok) )
 
 #save url !!!!!!!
@@ -104,6 +96,15 @@ res <- lapply(dirs, function(d) {
   
  unlink(zip_char)
  #write.table(mod_url, file= "data.csv", append = TRUE, sep = "\t", col.names = FALSE)
- return(mod_url)
-})
-save(res, file = "gallery/mod_urls.RData")
+ resultat <- data.frame(d,mod_url)
+
+ # names(resultat) <- names(data)
+ 
+
+  print(sp_name)
+  return(resultat)
+ 
+}))
+data <- rbind(data,new_res)
+
+save(data, file = "gallery/mod_urls.RData")
