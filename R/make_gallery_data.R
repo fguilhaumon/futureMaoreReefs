056b786362203391267be34df539bacc9bd478c9
mateoensc <mmoreau016@ensc.fr>
2022-07-05 14:37:03 +0400

    :zip: Create proper data

--- R/make_gallery_data.R
+++ R/make_gallery_data.R
@@ -3,15 +3,17 @@
 make_gallery_data <- function() {
   
   #list meta_data files
-  base_dir = "/data/fmr_data"
-  file <- list.dirs(base_dir, full.names = TRUE, recursive = FALSE)
-  data  <- lapply(file, function(){
-    
-    
-    
-  })
-  gg <- read.csv("/data/fmr_data/meta_data/fmr_jollyroger_23022022.csv")
-  
+  base_dir = "/data/fmr_data/meta_data"
+  data      <- lapply(list.files(base_dir, full.names = TRUE, recursive = FALSE),read.csv)
+  colnames(data[[2]]) <- c( "date" ,    "site",     "name",     "x",        "y",        "z",        "species",  "observer", "note")
+  data      <- do.call(rbind, data)
+  # Find and replace
+  data$name <- gsub("Tag ","tag_", data$name)
+  #Select row with tag
+  data      <- data[grep("tag_",data$name),]
+  data$date <- substr(data$date, 1, 10)
+  #Change date format
+  data$date <- strftime(strptime(data$date, "%m/%d/%Y", tz = "UTC"),"%d/%m/%Y")
   dates <- sapply(strsplit(gg$date, " "), "[[", 1)
   as.Date(dates)
   
